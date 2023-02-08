9d5138e7e919926b151f4e51a45605e111bc78e2
Charlie Gray <charliechasegray@gmail.com>
2023-02-08 16:08:39 -0700

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = () => { 
+  return tutorials.map(tutorial => titleCase(tutorial));
 }
+
+const titleCase = (tutorial) => tutorial.split(" ").map(function(word) {
+  return (word.charAt(0).toUpperCase() + word.slice(1));
+}).join(" ");
